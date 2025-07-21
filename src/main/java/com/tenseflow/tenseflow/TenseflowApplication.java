d743f2313fec8ea7a53dbbe608fc439fc5f8f58c
Denta Muhajir <denta.muhajir@binus.ac.id>
2025-07-21 19:31:36 +0700

    Initialize Spring Boot Project

---
+++ src/main/java/com/tenseflow/tenseflow/TenseflowApplication.java
@@ -0,0 +1,13 @@
+package com.tenseflow.tenseflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenseflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TenseflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tenseflow/tenseflow/TenseflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tenseflow.tenseflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenseflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
