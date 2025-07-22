8924b576d87c8377176322c443496e9d1a869c5b
Denta Muhajir <denta.muhajir@binus.ac.id>
2025-07-22 18:13:26 +0700

    Random category of partOfSpeech and store

--- src/main/java/com/tenseflow/PartOfSpeech/PartOfSpeechService.java
+++ src/main/java/com/tenseflow/PartOfSpeech/PartOfSpeechService.java
@@ -10,13 +10,27 @@ public class PartOfSpeechService {
 
     void generateWord(){
 
+        Integer randomNumber = (int) (Math.random() * 8) + 1;
+
+        //System.out.println("random number" + randomNumber);
         HashMap<String, String> partOfSpeechMap = listPartOfSpeech();
 
+        String getPartOfSpeech = "", getFile="";
+
+        Integer index = 1;
         for (Map.Entry<String, String> entry : partOfSpeechMap.entrySet()) {
-            String partOfSpeech = entry.getKey();
-            String filename = entry.getValue();
-            System.out.println(partOfSpeech + " => " + filename);
+            //String partOfSpeech = entry.getKey();
+            //String filename = entry.getValue();
+            //System.out.println("index " +index+ " "  + partOfSpeech + " => " + filename);
+            if(index.equals(randomNumber)){
+              getPartOfSpeech = entry.getKey();
+              getFile = entry.getValue();
+            }
+
+            index++;
         }
+
+        System.out.println("Random part of speech is " + getPartOfSpeech);
     }
 
     HashMap<String, String> listPartOfSpeech() {
