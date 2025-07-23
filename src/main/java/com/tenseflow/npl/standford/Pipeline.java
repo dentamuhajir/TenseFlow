f779b8b2b3c293f3306057cd0035611653790243
Denta Muhajir <denta.muhajir@binus.ac.id>
2025-07-23 22:45:17 +0700

    Add NLP pipeline

---
+++ src/main/java/com/tenseflow/npl/standford/Pipeline.java
@@ -0,0 +1,27 @@
+package com.tenseflow.npl.standford;
+
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+
+import java.util.Properties;
+
+// https://www.youtube.com/watch?v=D0LcPg9Jy08
+public class Pipeline {
+    private static StanfordCoreNLP stanfordCoreNLP;
+    private static Properties properties;
+    private static String propertiesName = "";
+
+    private Pipeline(){}
+
+    static {
+        properties = new Properties();
+        properties.setProperty("annotators", propertiesName);
+    }
+
+    public static StanfordCoreNLP getPipeline() {
+        if(stanfordCoreNLP == null) {
+            stanfordCoreNLP = new StanfordCoreNLP(properties);
+        }
+        return stanfordCoreNLP;
+    }
+
+}
